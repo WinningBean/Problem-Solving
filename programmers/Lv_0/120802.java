3790c3c31d9fa38f4b1ecce74fa7f136b38040ee
Seungbeen Wee <wsb0722@naver.com>
2022-11-07 02:53:44 +0900

    파일 정리
    백준 허브 사용으로 기존 파일 삭제

--- programmers/Lv_0/120802.java
+++
@@ -1,6 +0,0 @@
-// 두 수의 합
-class Solution {
-    public int solution(int num1, int num2) {
-        return num1 + num2;
-    }
-}
--- programmers/Lv_0/120806.java
+++
@@ -1,7 +0,0 @@
-// 두 수의 나눗셈
-class Solution {
-    public int solution(int num1, int num2) {
-        float quotient = (float) num1 / num2;
-        return (int) (quotient * 1000);
-    }
-}
