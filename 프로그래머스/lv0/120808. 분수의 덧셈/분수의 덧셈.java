6986e26f2d559bc622dc2e58f3049d7bdc8a1ed3
SeungBeen Wee <WinningBean@gmail.com>
2022-11-07 02:50:48 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int numerator = denum1 * num2 + denum2 * num1;
+        int denominator = num1 * num2;
+        int gcd = gcd(denominator, numerator);
+
+        numerator /= gcd;
+        denominator /= gcd;
+
+        int[] answer = {numerator, denominator};
+        return answer;
+    }
+
+    private int gcd(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return gcd(num2, num1 % num2);
+    }
+}
