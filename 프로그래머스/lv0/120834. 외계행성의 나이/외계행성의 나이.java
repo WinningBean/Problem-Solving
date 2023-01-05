5328fdea5667f09b0dc717f463fec284e8cf6689
SeungBeen Wee <WinningBean@gmail.com>
2023-01-05 11:18:07 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,9 +1,10 @@
 class Solution {
     public String solution(int age) {
+        char[] ageCharArr = String.valueOf(age).toCharArray();
         StringBuilder sb = new StringBuilder();
-        String.valueOf(age).chars().forEach(
-            i -> sb.append((char)(i + 49))
-        );
+        for (char ageChar : ageCharArr) {
+            sb.append((char)(ageChar + 49));
+        }
 
         return sb.toString();
     }
