8d846ff6069e261cd0e43784ba6e4b1bc8bdd679
SeungBeen Wee <WinningBean@gmail.com>
2023-12-14 21:44:54 +0900

    [level unrated] Title: 개미 군단, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,20 @@
+class Solution {
+    static final int GENERAL_POWER = 5;
+    static final int SOLDIER_POWER = 3;
+    static final int WORKER_POWER = 1;
+
+    public int solution(int hp) {
+        int answer = 0;
+
+        answer += hp / GENERAL_POWER;
+        hp %= GENERAL_POWER;
+
+        answer += hp / SOLDIER_POWER;
+        hp %= SOLDIER_POWER;
+
+        answer += hp / WORKER_POWER;
+        hp %= WORKER_POWER;
+        
+        return answer;
+    }
+}
