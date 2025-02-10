f5beb7d92e605687525c495bfe190ffdba4f4d41
SeungBeen Wee <WinningBean@gmail.com>
2025-02-10 17:59:26 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(
+        """
+                 ,r'\"7
+        r`-_   ,'  ,/
+         \\. ". L_r'
+           `~\\/
+              |
+              |
+        """
+        );
+    }
+}
