821bd1ad06d5a98287b367f1fa8abf0ad530f239
SeungBeen Wee <WinningBean@gmail.com>
2025-02-16 02:01:46 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 16300 ms, Memory: 102280 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        final int MAX_ABS_NUM = 1000000;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] numStates = new int[MAX_ABS_NUM * 2 + 1];
+
+        for (int i = 0; i < N; i++) {
+            numStates[Integer.parseInt(br.readLine()) + MAX_ABS_NUM]++;
+        }
+
+        int outLine = 0;
+        for (int i = 0; i < numStates.length; i++) {
+            for (int j = 0; j < numStates[i]; j++) {
+                sb.append(i - MAX_ABS_NUM).append("\n");
+                if (outLine++ == N) break;
+            }
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
