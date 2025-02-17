416941b67cd21788c0ada8e6178a4866512b6537
SeungBeen Wee <WinningBean@gmail.com>
2025-02-17 23:41:51 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18788 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int[][] LCS = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    LCS[i][j] = LCS[i - 1][j - 1] + 1;
+                } else {
+                    LCS[i][j] = Math.max(LCS[i][j - 1], LCS[i - 1][j]);
+                }
+            }
+        }
+
+        sb.append(LCS[str1.length()][str2.length()]);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
