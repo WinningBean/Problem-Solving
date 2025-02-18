eda4285b1de7dff597b45807d78def543c96c829
SeungBeen Wee <WinningBean@gmail.com>
2025-02-18 17:40:46 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -13,18 +13,20 @@ class Main {
             stairsScores[i] = Integer.parseInt(br.readLine());
         }
 
-        int[][] maxScores = new int[2][stairsCnt];
-        maxScores[0][0] = maxScores[1][0] = stairsScores[0];
+        int[] maxScores = new int[stairsCnt];
+        maxScores[0] = stairsScores[0];
         if (stairsCnt > 1) {
-            maxScores[0][1] = stairsScores[0] + stairsScores[1];
-            maxScores[1][1] = stairsScores[1];
+            maxScores[1] = stairsScores[0] + stairsScores[1];
         }
-        for (int i = 2; i < stairsCnt; i++) {
-            maxScores[0][i] = maxScores[1][i - 1] + stairsScores[i];
-            maxScores[1][i] = Math.max(maxScores[0][i - 2], maxScores[1][i - 2]) + stairsScores[i];
+        if (stairsCnt > 2) {
+            maxScores[2] = Math.max(stairsScores[0], stairsScores[1]) + stairsScores[2];
         }
 
-        sb.append(Math.max(maxScores[0][stairsCnt - 1], maxScores[1][stairsCnt - 1]));
+        for (int i = 3; i < stairsCnt; i++) {
+            maxScores[i] = Math.max(maxScores[i - 2], maxScores[i - 3] + stairsScores[i - 1]) + stairsScores[i];
+        }
+
+        sb.append(maxScores[stairsCnt - 1]);
 
         System.out.println(sb);
         br.close();
