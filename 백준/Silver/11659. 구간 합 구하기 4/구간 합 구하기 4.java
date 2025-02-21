1c75c4e9da2ef5a344c6d3095e4eb53d4cb335bb
SeungBeen Wee <WinningBean@gmail.com>
2025-02-21 17:03:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 576 ms, Memory: 60160 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -8,17 +8,21 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int[] NM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
         int[] numArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        int[] prefixSumArr = new int[NM[0] + 1];
-        for (int i = 1; i <= NM[0]; i++) {
-            prefixSumArr[i] = prefixSumArr[i - 1] + numArr[i - 1];
+        int[] prefixSumArr = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            prefixSumArr[i + 1] = prefixSumArr[i] + numArr[i];
         }
 
-        while (NM[1]-- > 0) {
-            int[] problemInfo = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            sb.append(prefixSumArr[problemInfo[1]] - prefixSumArr[problemInfo[0] - 1]).append("\n");
+        while (M-- > 0) {
+            String[] problemInfo = br.readLine().split(" ");
+            int startIdx = Integer.parseInt(problemInfo[0]);
+            int endIdx = Integer.parseInt(problemInfo[1]);
+            sb.append(prefixSumArr[endIdx] - prefixSumArr[startIdx - 1]).append("\n");
         }
 
         System.out.print(sb);
