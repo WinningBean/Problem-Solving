ecffb321c5bebc86912a72f6fbb1326ffe0d5106
SeungBeen Wee <WinningBean@gmail.com>
2025-02-23 04:23:14 +0900

    [Silver III] Title: Four Squares, Time: 132 ms, Memory: 15008 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -8,21 +8,25 @@ class Main {
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
+        int result = 1;
 
-        int[] squareNums = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (Math.sqrt(i) % 1 == 0) {
-                squareNums[i] = 1;
-            } else {
-                squareNums[i] = 4;
-                for (int j = 1; j < Math.sqrt(i); j++) {
-                    squareNums[i] = Math.min(squareNums[i], squareNums[i - (int) Math.pow(j, 2)] + 1);
-                    if (squareNums[i] == 2) break;
+        if (Math.sqrt(n) % 1 > 0) {
+            int[] squareNums = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                if (Math.sqrt(i) % 1 == 0) {
+                    squareNums[i] = 1;
+                } else {
+                    squareNums[i] = 4;
+                    for (int j = 1; j < Math.sqrt(i); j++) {
+                        squareNums[i] = Math.min(squareNums[i], squareNums[i - j * j] + 1);
+                        if (squareNums[i] == 2) break;
+                    }
                 }
             }
+            result = squareNums[n];
         }
 
-        sb.append(squareNums[n]);
+        sb.append(result);
 
         System.out.println(sb);
         br.close();
