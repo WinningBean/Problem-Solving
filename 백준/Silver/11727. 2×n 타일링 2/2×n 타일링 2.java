85c9910391a042d87db034378e565a725b627113
SeungBeen Wee <WinningBean@gmail.com>
2025-02-22 21:38:49 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        final int MOD_NUM = 10007;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] sumArr = new int[n + 1];
+        sumArr[1] = 1;
+        if (n > 1) {
+            sumArr[2] = 3;
+
+            for (int i = 3; i <= n; i++) {
+                sumArr[i] = sumArr[i - 2] * 2 + sumArr[i - 1];
+    
+                if (sumArr[i] > MOD_NUM) {
+                    sumArr[i] %= MOD_NUM;
+                }
+            }
+        }
+
+        sb.append(sumArr[n]);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
