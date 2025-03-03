4b8b2b4d471780f75044b069435b2a3076220d6f
SeungBeen Wee <WinningBean@gmail.com>
2025-03-03 21:13:31 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        sb.append(A + B + C < 22 ? 1 : 0);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
