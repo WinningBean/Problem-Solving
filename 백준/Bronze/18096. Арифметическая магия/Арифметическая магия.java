b7b5890fad6028808eca6be2f91877d18052ad92
SeungBeen Wee <WinningBean@gmail.com>
2025-03-05 00:26:58 +0900

    [Bronze V] Title: Арифметическая магия, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String N = br.readLine();
+        sb.append(1);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
