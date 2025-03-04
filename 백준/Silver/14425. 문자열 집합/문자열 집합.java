3b9f486c1e8210fa05ce73a2f7de7b9b45d8d2ed
SeungBeen Wee <WinningBean@gmail.com>
2025-03-04 18:29:42 +0900

    [Silver IV] Title: 문자열 집합, Time: 372 ms, Memory: 31228 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+        Set<String> strSet = new HashSet<>();
+
+        while (N-- > 0) {
+            strSet.add(br.readLine());
+        }
+
+        int result = 0;
+        while (M-- > 0) {
+            if (strSet.contains(br.readLine())) result++;
+        }
+        
+        sb.append(result);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
