396623d3a9e1660956dc5d07935d5f65b706f0d4
SeungBeen Wee <WinningBean@gmail.com>
2025-03-04 23:54:33 +0900

    [Bronze V] Title: Zadanie próbne, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -7,9 +7,7 @@ class Main {
         StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
-        for (int i = str.length() - 1; i >= 0; i--) {
-            sb.append(str.charAt(i));
-        }
+        sb.append(str).reverse();
 
         System.out.println(sb);
         br.close();
