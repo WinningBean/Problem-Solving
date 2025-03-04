cb8605224dd8b7a2e47ec6661f7ed8b634cc633e
SeungBeen Wee <WinningBean@gmail.com>
2025-03-05 00:34:45 +0900

    [Bronze V] Title: Rats, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] info = br.readLine().split(" ");
+        int n1 = Integer.parseInt(info[0]);
+        int n2 = Integer.parseInt(info[1]);
+        int n12 = Integer.parseInt(info[2]);
+
+        sb.append((n1 + 1) * (n2 + 1) / (n12 + 1) - 1);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
