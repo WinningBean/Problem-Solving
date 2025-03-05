2e85d63bd08f9765156d5eeaa6d324478129fe8f
SeungBeen Wee <WinningBean@gmail.com>
2025-03-05 20:37:04 +0900

    [Bronze V] Title: 身長 (Height), Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        sb.append(B - A);
+
+        System.out.print(sb);
+        br.close();
+    }
+}
