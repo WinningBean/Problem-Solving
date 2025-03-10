0598cf11f4beeded91b1e9e4ec93761e1bf53880
SeungBeen Wee <WinningBean@gmail.com>
2025-03-10 21:36:05 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.ts
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.ts
@@ -5,8 +5,6 @@ const input = fs.readFileSync(filePath, 'utf8').toString().split("\n");
 let S = parseInt(input[0]);
 let F = parseInt(input[1]);
 
-let result: string;
-if (S > F) result = "flight";
-else result = "high speed rail";
+let result: string = S > F ? "flight" : "high speed rail";
 
 console.log(result);
