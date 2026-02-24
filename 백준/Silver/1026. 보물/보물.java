b6988cafc842f705c71a443e8206c7f883f54c18
SeungBeen Wee <WinningBean@gmail.com>
2026-02-25 00:04:06 +0900

    [Silver IV] Title: 보물, Time: 112 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main{
+    static int[][] combination = new int[31][31];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int answer = 0;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < N; i++) {
+            answer += A[i] * B[N - i - 1];
+        }
+
+        System.out.print(answer);
+        
+        br.close();
+    }
+}
