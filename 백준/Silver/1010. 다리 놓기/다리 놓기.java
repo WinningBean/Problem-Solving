7878a0098ed9097f712824d03427a43c4cfa0565
SeungBeen Wee <WinningBean@gmail.com>
2026-02-24 21:46:29 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String info[] = br.readLine().split(" ");
+            int N = Integer.parseInt(info[0]);
+            int M = Integer.parseInt(info[1]);
+
+            sb.append(getCombination(M, N))
+                .append("\n");
+        }
+
+        System.out.print(sb);
+        
+        br.close();
+    }
+
+    static long getCombination(int n, int r) {
+        if (r > 1) {
+            return n * getCombination(n - 1, r - 1) / r;
+        }
+
+        long value = 1;
+        for (int i = n; i > n - r; i--) {
+            value *= i;
+        }
+        return value;
+    }
+}
