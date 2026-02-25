dc31ad78a50ab6954c41abc299d5e813cc21d985
SeungBeen Wee <WinningBean@gmail.com>
2026-02-26 02:28:44 +0900

    [Bronze I] Title: ISBN, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/14626. ISBN/ISBN.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] ISBN = br.readLine().toCharArray();
+
+        int sum = 0;
+        int damagedMultiply = 0;
+        for (int i = 0; i < 13; i++) {
+            int multiply = i % 2 == 0 ? 1 : 3;
+
+            if (ISBN[i] == '*') {
+                damagedMultiply = multiply;
+            } else {
+                int num = ISBN[i] - '0';
+                sum += multiply * num;
+            }
+        }
+
+        int rest = 10 - sum % 10;
+        int answer = rest % 10;
+        if (answer > 0 && damagedMultiply == 3) {
+            for (int i = 1; i < 10; i++) {
+                if (rest == i * damagedMultiply % 10) {
+                    answer = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.print(answer);
+        br.close();
+    }
+}
