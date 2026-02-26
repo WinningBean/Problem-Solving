d5b46814e914133ca38522cd16b658898eda5ddf
SeungBeen Wee <WinningBean@gmail.com>
2026-02-27 03:27:27 +0900

    [Silver II] Title: 과일 탕후루, Time: 312 ms, Memory: 37864 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
 
 class Main{
     public static void main(String[] args) throws IOException {
@@ -10,53 +9,37 @@ class Main{
 
         int N = Integer.parseInt(br.readLine());
         int[] fruits = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] fruitSequenceCounts = getSequenceCounts(fruits);
 
         final int MAX_FRUIT = 2;
-        int maxCount = 0;
-        int checkIdx = 0;
 
-        while (checkIdx < N) {
-            HashSet<Integer> fruitSet = new HashSet<>();
-            int count = 0;
+        int count = 0;
+        int start = 0;
+        while (start < N) {
+            boolean[] usedFruits = new boolean[10];
+            int end = start;
+            int nextStart = end;
+            int used = 0;
 
-            int nowIdx = checkIdx;
-            while (nowIdx < N) {
-                if (!fruitSet.contains(fruits[nowIdx])) {
-                    if (fruitSet.size() == MAX_FRUIT) break;
-                    fruitSet.add(fruits[nowIdx]);
+            while (end < N) {
+                int nowFruit = fruits[end];
+                if (!usedFruits[nowFruit]) {
+                    if (used == MAX_FRUIT) break;
+                    used++;
                 }
-
-                checkIdx = nowIdx;
-                count += fruitSequenceCounts[nowIdx];
-                nowIdx += fruitSequenceCounts[nowIdx];
-            }
-
-            if (maxCount < count) {
-                maxCount = count;
+                if (fruits[nextStart] != nowFruit) {
+                    nextStart = end;
+                }
+                usedFruits[nowFruit] = true;
+                end++;
             }
             
-            if (nowIdx == N) break;
+            count = Math.max(count, end - start);
+
+            if (end == N) break;
+            start = nextStart;
         }
 
-        System.out.print(maxCount);
+        System.out.print(count);
         br.close();
-    }
-
-    static int[] getSequenceCounts(int[] arr) {
-        int length = arr.length;
-        int[] sequenceCounts = new int[length];
-
-        int checkIdx = 0;
-        while (checkIdx < length) {
-            int startIdx = checkIdx;
-            int num = arr[checkIdx];
-            while (checkIdx < length && arr[checkIdx] == num) {
-                checkIdx++;
-                sequenceCounts[startIdx]++;
-            }
-        }
-
-        return sequenceCounts;
     }
 }
