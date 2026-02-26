6334d645fc4ca97d865d8964e9c664b55770c1b3
SeungBeen Wee <WinningBean@gmail.com>
2026-02-27 01:00:35 +0900

    [Silver II] Title: 과일 탕후루, Time: 396 ms, Memory: 65760 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -10,40 +10,53 @@ class Main{
 
         int N = Integer.parseInt(br.readLine());
         int[] fruits = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] fruitSequenceCounts = getSequenceCounts(fruits);
+
         final int MAX_FRUIT = 2;
-
         int maxCount = 0;
-        int startIdx = 0;
-        while (startIdx < N) {
-            HashSet<Integer> choiceFruitSet = new HashSet<>();
+        int checkIdx = 0;
 
+        while (checkIdx < N) {
+            HashSet<Integer> fruitSet = new HashSet<>();
             int count = 0;
-            int checkIdx = startIdx;
-            while (checkIdx < N) {
-                if (!choiceFruitSet.contains(fruits[checkIdx])) {
-                    // 종류가 MAX_FRUIT룰 넘어가면 no count
-                    if (choiceFruitSet.size() == MAX_FRUIT) break;
-                    choiceFruitSet.add(fruits[checkIdx]);
-                    startIdx = checkIdx;
+
+            int nowIdx = checkIdx;
+            while (nowIdx < N) {
+                if (!fruitSet.contains(fruits[nowIdx])) {
+                    if (fruitSet.size() == MAX_FRUIT) break;
+                    fruitSet.add(fruits[nowIdx]);
                 }
-                
-                // 마지막 과일 시작 점을 다음 시작점으로
-                if (choiceFruitSet.size() == MAX_FRUIT && fruits[checkIdx - 1] != fruits[checkIdx]) {
-                    startIdx = checkIdx;
-                }
-                
-                count++;
-                checkIdx++;
+
+                checkIdx = nowIdx;
+                count += fruitSequenceCounts[nowIdx];
+                nowIdx += fruitSequenceCounts[nowIdx];
             }
 
             if (maxCount < count) {
                 maxCount = count;
             }
-
-            if (checkIdx == N) break;
+            
+            if (nowIdx == N) break;
         }
 
         System.out.print(maxCount);
         br.close();
+    }
+
+    static int[] getSequenceCounts(int[] arr) {
+        int length = arr.length;
+        int[] sequenceCounts = new int[length];
+
+        int checkIdx = 0;
+        while (checkIdx < length) {
+            int startIdx = checkIdx;
+            int num = arr[checkIdx];
+            while (checkIdx < length && arr[checkIdx] == num) {
+                checkIdx++;
+                sequenceCounts[startIdx]++;
+            }
+        }
+
+        return sequenceCounts;
     }
 }
