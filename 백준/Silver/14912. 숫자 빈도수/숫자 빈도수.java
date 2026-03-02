46c406d829267a23fa12c4ca018444e6ced8dcd8
SeungBeen Wee <WinningBean@gmail.com>
2026-03-03 02:53:23 +0900

    [Silver V] Title: 숫자 빈도수, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
@@ -13,23 +13,17 @@ class Main{
         boolean dIsZero = d == 0;
         int count = 0;
 
-        // 1부터 자릿수 높여서 체크
         for (int placeValue = 1; placeValue <= n; placeValue *= 10) {
-            int lowNum = n % placeValue; // 아래 자릿수 숫자
-            int nowNum = (n / placeValue) % 10; // 현재 자릿수
-            int highNum = n / (placeValue * 10); // 위 자릿수 숫자
+            int lowNum = n % placeValue;
+            int nowNum = (n / placeValue) % 10;
+            int highNum = n / (placeValue * 10);
             
-            // d가 0인 경우 앞자리가 0인 경우는 없으니 현재 자릿수 크기만큼 제거
-            // -> highNum을 줄여서 미리 세팅
             if (dIsZero) highNum--;
 
-            // 현재 자릿수에 d가 나타날 경우는 위 자릿수 만큼 * 현재 자릿수 크기
             count += highNum * placeValue;
             if (nowNum == d) {
-                // 현재 자릿수가 d와 같을 경우 아래 자릿수 만큼 나타남 + 1(0의 경우도 체크)
                 count += lowNum + 1;
             } else if (nowNum > d) {
-                // 현재 자릿수가 d보다 클 경우 현재 자릿수 크기 만큼 나타남
                 count += placeValue;
             }
         }
