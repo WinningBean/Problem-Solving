bc8b718d69ec25e77591cbe847149e01595f016a
SeungBeen Wee <WinningBean@gmail.com>
2026-03-03 02:46:57 +0900

    [Silver V] Title: 숫자 빈도수, Time: 100 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
@@ -1,48 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
-        char[] nArr = input[0].toCharArray();
+        int n = Integer.parseInt(input[0]);
         int d = Integer.parseInt(input[1]);
 
-        boolean isZero = d == 0;
-        int digit = nArr.length;
+        boolean dIsZero = d == 0;
         int count = 0;
 
-        // 1의 자릿수 이전까지 계산
-        for (int i = 0; i < digit - 1; i++) {
-            int num = nArr[i] - '0';
-            int nowDigit = digit - i;
+        // 1부터 자릿수 높여서 체크
+        for (int placeValue = 1; placeValue <= n; placeValue *= 10) {
+            int lowNum = n % placeValue; // 아래 자릿수 숫자
+            int nowNum = (n / placeValue) % 10; // 현재 자릿수
+            int highNum = n / (placeValue * 10); // 위 자릿수 숫자
+            
+            // d가 0인 경우 앞자리가 0인 경우는 없으니 현재 자릿수 크기만큼 제거
+            // -> highNum을 줄여서 미리 세팅
+            if (dIsZero) highNum--;
 
-            // 현재 자릿수에서 한 숫자가 나오는 빈도수
-            int frequency = (nowDigit - 1) * (int) Math.pow(10, nowDigit - 2);
-            // 현재 자릿수의 숫자만큼 빈도수가 나온다.
-            count += num * frequency;
-            // 앞자리가 0인 경우는 없으므로 d가 0인 경우 다음 자릿수 * 10만큼 제외
-            if (isZero) {
-                count -= (int) Math.pow(10, nowDigit - 2);
-            }
-
-            if (num == d) {
-                // 현재 자릿수가 d와 같다면 그 아래 수만큼 count + 1(0 count)
-                count += Integer.parseInt(new String(Arrays.copyOfRange(nArr, i + 1, digit))) + 1;
-            } else if (num > d && (!isZero || (isZero && i > 0))) {
-                // 현재 자릿수가 d보다 크면 해당 자릿수 크기만큼 count
-                // 앞자리가 0인 경우는 없으므로 d가 0인 경우 맨 앞 자릿수는 제외
-                count += (int) Math.pow(10, nowDigit - 1);
+            // 현재 자릿수에 d가 나타날 경우는 위 자릿수 만큼 * 현재 자릿수 크기
+            count += highNum * placeValue;
+            if (nowNum == d) {
+                // 현재 자릿수가 d와 같을 경우 아래 자릿수 만큼 나타남 + 1(0의 경우도 체크)
+                count += lowNum + 1;
+            } else if (nowNum > d) {
+                // 현재 자릿수가 d보다 클 경우 현재 자릿수 크기 만큼 나타남
+                count += placeValue;
             }
         }
-
-        // 1의 자릿수 따로 계산
-        // 1의 자릿수가 d보다 같거나 크면 한 번 더 count
-        int digitOneNum = nArr[digit - 1] - '0';
-        if (digitOneNum >= d) count++;
 
         System.out.print(count);
         br.close();
