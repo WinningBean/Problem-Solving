735913e57cbd9ef1c9cb2e0ef190ea56f75b641a
SeungBeen Wee <WinningBean@gmail.com>
2026-03-04 00:52:19 +0900

    [Silver II] Title: 마인크래프트, Time: 320 ms, Memory: 38484 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main{
-    static final int MAX_GROUND_LEVEL = 256;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -13,8 +11,8 @@ class Main{
         int N = Integer.parseInt(input[0]);
         int B = Integer.parseInt(input[2]);
 
-        int[] ground = new int[MAX_GROUND_LEVEL + 1];
-        int minLevel = MAX_GROUND_LEVEL, maxLevel = 0;
+        int[] ground = new int[257];
+        int minLevel = 256, maxLevel = 0;
 
         for (int i = 0; i < N; i++) {
             String[] blocks = br.readLine().split(" ");
@@ -29,28 +27,24 @@ class Main{
         int minTime = Integer.MAX_VALUE;
         int flatLevel = 0;
 
-        // 만들어 질 수 있는 모든 레벨
+        // 만들어 질 수 있는 모든 레벨 체크
         for (int nowLevel = minLevel; nowLevel <= maxLevel; nowLevel++) {
-            int nowTime = 0;
-            int nowInventory = B;
-
-            // 가장 낮은 레벨부터 현재 레벨 전까지
+            // 가장 낮은 레벨부터 현재 레벨 전까지 더해야 할 블록 체크
+            int addBlocks = 0;
             for (int lowLevel = minLevel; lowLevel < nowLevel; lowLevel++) {
-                int addBlocks = (nowLevel - lowLevel) * ground[lowLevel];
-                nowTime += addBlocks;
-                nowInventory -= addBlocks;
+                addBlocks += (nowLevel - lowLevel) * ground[lowLevel];
             }
 
-            // 다음 레벨부터 가장 높은 레벨까지
-            for (int highLevel = nowLevel + 1; highLevel <=maxLevel; highLevel++) {
-                int removeBlocks = (highLevel - nowLevel) * ground[highLevel];
-                nowTime += removeBlocks * 2;
-                nowInventory += removeBlocks;
+            // 다음 레벨부터 가장 높은 레벨까지 제거해야 할 블록 체크
+            int removeBlocks = 0;
+            for (int highLevel = nowLevel + 1; highLevel <= maxLevel; highLevel++) {
+                removeBlocks += (highLevel - nowLevel) * ground[highLevel];
             }
 
             // 현재 레벨로 만들기 위한 블록이 충분하지 않다면 다음 레벨 체크
-            if (nowInventory < 0) continue;
-            
+            if (removeBlocks + B < addBlocks) continue;
+
+            int nowTime = addBlocks + removeBlocks * 2;
             if (nowTime <= minTime) {
                 minTime = nowTime;
                 flatLevel = nowLevel;
