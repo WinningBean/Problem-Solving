1e4581dfe28ccbf68626cb971f4623801c349ff9
SeungBeen Wee <WinningBean@gmail.com>
2026-03-05 06:50:29 +0900

    [Silver I] Title: IOIOI, Time: 256 ms, Memory: 23064 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 class Main{
     public static void main(String[] args) throws IOException {
@@ -11,19 +10,34 @@ class Main{
         int M = Integer.parseInt(br.readLine());
         char[] S = br.readLine().toCharArray();
 
+        boolean[] canP = getCanP(S, M);
+        int count = getCanPnCount(N, M, canP);
+
+        System.out.print(count);
+        br.close();
+    }
+
+    /// Pn을 만들 수 있는 경우의 수 반환
+    static int getCanPnCount(int N, int M, boolean[] canP) {
         int PnLength = 2 * N + 1;
         int count = 0;
+        int start = 0, end = 0;
 
-        // Pn이 가능한 만큼만 loop
-        for (int i = 0; i <= M - PnLength; i++) {
-            char[] nowS = Arrays.copyOfRange(S, i, i + PnLength);
+        // Pn이 만들어 질 수 있는 길이만큼 loop
+        while (start <= M - PnLength) {
+            if (!canP[start]) {
+                start++;
+                end++;
+                continue;
+            }
             boolean isPn = true;
 
-            for (int j = 0; j <= PnLength / 2; j += 2) {
-                if (nowS[j] != 'I'
-                    || nowS[PnLength - j - 1] != 'I'
-                    || nowS[j + 1] != 'O'
-                    || nowS[PnLength - (j + 1) - 1] != 'O') {
+            // 확인된 부분이 PnLength보다 작을 때 loop
+            while (end - start + 3 < PnLength) {
+                if (canP[end + 2]) {
+                    // 최소 단위는 3글자 -> end + 2가 P문자열인지 확인
+                    end += 2;
+                } else {
                     isPn = false;
                     break;
                 }
@@ -31,11 +45,26 @@ class Main{
 
             if (isPn) {
                 count++;
-                i++; // Pn의 경우 바로 다음 문자열은 Pn이 될 수 없음
+                start += 2; // P문자열이면 바로 다음은 P문자열이 될 수 없음
+            } else {
+                start = ++end;
             }
         }
 
-        System.out.print(count);
-        br.close();
+        return count;
+    }
+
+    /// P의 최소 단위 'IOI' 가능 여부 반환
+    static boolean[] getCanP(char[] S, int M) {
+        boolean[] canP = new boolean[M];
+
+        for (int i = 0; i < M - 2; i++) {
+            if (S[i] == 'I' && S[i + 1] == 'O' && S[i + 2] == 'I') {
+                canP[i] = true;
+                i++; // P문자열이면 바로 다음은 P문자열이 될 수 없음
+            }
+        }
+
+        return canP;
     }
 }
