e846244329be525eccee2e792e207fbd68127b94
SeungBeen Wee <WinningBean@gmail.com>
2026-03-05 03:19:59 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 112 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,90 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 class Main{
-    static int uesrLength;
-    static User[] users;
+    static int N;
+    static int[][] baconNums;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
-        uesrLength = Integer.parseInt(input[0]);
+        N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
 
-        users = new User[uesrLength + 1];
-        for (int i = 1; i <= uesrLength; i++) {
-            users[i] = new User(i, uesrLength);
+        baconNums = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(baconNums[i], Integer.MAX_VALUE);
+            baconNums[i][i] = 0;
         }
-
+        
         while (M-- > 0) {
             String[] connection = br.readLine().split(" ");
-            User userA = users[Integer.parseInt(connection[0])];
-            User userB = users[Integer.parseInt(connection[1])];
-
-            userA.addFriend(userB);
-            userB.addFriend(userA);
+            int A = Integer.parseInt(connection[0]);
+            int B = Integer.parseInt(connection[1]);
+            baconNums[A][B] = baconNums[B][A] = 1;
         }
 
-        int minBaconNum = Integer.MAX_VALUE;
-        int minBaconNumUser = 0;
-        for (int i = 1; i <= uesrLength; i++) {
-            setUserBaconNums(i);
-            int baconNum = users[i].getBaconNum();
+        setBaconNums();
 
-            if (baconNum < minBaconNum) {
+        int minBaconNum = Integer.MAX_VALUE;
+        int minBaconNumId = 0;
+        for (int i = 1; i <= N; i++) {
+            int baconNum = 0;
+            for (int j = 1; j <= N; j++) {
+                baconNum += baconNums[i][j];
+            }
+            if (minBaconNum > baconNum) {
                 minBaconNum = baconNum;
-                minBaconNumUser = i;
+                minBaconNumId = i;
             }
         }
 
-        System.out.print(minBaconNumUser);
+        System.out.print(minBaconNumId);
         br.close();
     }
 
-    static void setUserBaconNums(int id) {
-        boolean[] visited = new boolean[uesrLength + 1];
-        User user = users[id];
-        Queue<User> userQueue = new LinkedList<User>();
-        userQueue.add(user);
-        visited[id] = true;
-
-        while (!userQueue.isEmpty()) {
-            User connectUser = userQueue.poll();
-
-            for (int i = 1; i <= uesrLength; i++) {
-                // 방문 안 한 직접 친구인 경우를 통해서만 관계 계산
-                if (visited[i] || connectUser.baconNums[i] != 1) continue;
-                visited[i] = true;
-                userQueue.add(users[i]);
-                // 현재 i는 연결된 user와 관계 + 1
-                user.baconNums[i] = user.baconNums[connectUser.id] + 1;
+    static void setBaconNums() {
+        // 거쳐가는 정점
+        for (int k = 1; k <= N; k++) {
+            // 시작점
+            for (int i = 1; i <= N; i++) {
+                // 거쳐가지 못하는 경우 패스
+                if (baconNums[k][i] == Integer.MAX_VALUE || k == i) continue;
+                // 끝점
+                for (int j = 1; j <= N; j++) {
+                    // 거쳐가지 못하는 경우 패스
+                    if (baconNums[k][j] == Integer.MAX_VALUE || i == j) continue;
+                    baconNums[i][j] = Math.min(baconNums[i][j], baconNums[i][k] + baconNums[k][j]);
+                }
             }
         }
-    }
-}
-
-class User {
-    int id;
-    int[] baconNums;
-
-    User(int id, int userNum) {
-        this.id = id;
-        baconNums = new int[userNum + 1];
-    }
-
-    void addFriend(User friend) {
-        baconNums[friend.id] = 1;
-    }
-
-    int getBaconNum() {
-        int sum = 0;
-        for (int i = 1; i < baconNums.length; i++) {
-            sum += baconNums[i];
-        }
-        return sum;
     }
 }
